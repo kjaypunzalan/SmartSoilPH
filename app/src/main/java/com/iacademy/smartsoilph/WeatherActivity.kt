fa9301e348278c2484bcd508dccdfddb7ee374ad
aLai502 <104610544+aLai502@users.noreply.github.com>
2024-01-12 23:43:28 +0800

    Weather Activity & Navigation Menu Update

---
+++ app/src/main/java/com/iacademy/smartsoilph/WeatherActivity.kt
@@ -0,0 +1,11 @@
+package com.iacademy.smartsoilph
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.v2_activity_weather)
+    }
+}
