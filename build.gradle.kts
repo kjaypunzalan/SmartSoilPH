75d62964d8471d83993bbedd7b65caa261398ecf
kjaypunzalan <karljohn.punzalan@gmail.com>
2024-01-13 09:33:00 +0800

    Revert "Merge pull request #1 from kjaypunzalan/pat-branch"
    This reverts commit 06490a9991bea2695e0aa70217f4cf440ce41092, reversing
    changes made to 03c924005d76533b466a049ff45f7f231a47da69.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ buildscript {
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
 }
