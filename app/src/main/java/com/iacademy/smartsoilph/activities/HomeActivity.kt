12b4a79f7d381b43bb2e79c4723d18e7e5c65060
kjaypunzalan <karljohn.punzalan@gmail.com>
2024-02-01 13:59:00 +0800

    implemented Pat's home page

--- app/src/main/java/com/iacademy/smartsoilph/activities/HomeActivity.kt
+++ app/src/main/java/com/iacademy/smartsoilph/activities/HomeActivity.kt
@@ -4,35 +4,80 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
 import com.iacademy.smartsoilph.R
 import com.iacademy.smartsoilph.models.DatabaseHelper
+import com.iacademy.smartsoilph.models.FirebaseModel
 
 class HomeActivity : AppCompatActivity() {
+
+    //declare layout variables
+    private lateinit var btnSoil: CardView
+    private lateinit var btnWeather: CardView
+    private lateinit var btnReports: CardView
+    private lateinit var btnManual: CardView
+    private lateinit var tvUsername: TextView
+
+    //declare Firebase variables
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
         // declare + initialize variables
-        val btnSoil = findViewById<Button>(R.id.soil_card);
-        val btnWeather = findViewById<Button>(R.id.weather_card);
-        val btnReports = findViewById<Button>(R.id.reports_card);
-//        val btnManual = findViewById<Button>(R.id.btnManual);
+        btnSoil = findViewById<CardView>(R.id.soil_card);
+        btnWeather = findViewById<CardView>(R.id.weather_card);
+        btnReports = findViewById<CardView>(R.id.reports_card);
+        btnManual = findViewById<CardView>(R.id.manual_card);
+        tvUsername = findViewById<TextView>(R.id.tv_username)
 
         // set click listeners for buttons
         setButtonClickListener(btnSoil, SoilActivity::class.java)
         setButtonClickListener(btnWeather, WeatherActivity::class.java)
-        setButtonClickListener(btnReports, RecommendationHistoryActivity::class.java)
-        setButtonClickListener(btnManual, LoadScreenActivity::class.java)
+        setButtonClickListener(btnReports, ReportsActivity::class.java)
+        setButtonClickListener(btnManual, RecommendationHistoryActivity::class.java)
 
+        fetchUsername()
     }
 
     //Button Function
-    private fun setButtonClickListener(button: Button, activityClass: Class<*>) {
+    private fun setButtonClickListener(button: CardView, activityClass: Class<*>) {
         button.setOnClickListener {
             val intent = Intent(this, activityClass)
             startActivity(intent)
         }
+    }
+
+    private fun fetchUsername() {
+        // Get FirebaseDatabase Reference
+        val firebaseDB = Firebase.database.getReference("SmartSoilPH").child("Users").child(auth.currentUser!!.uid)
+
+        //Get User and Soil Reference
+        val userReference = firebaseDB.child("UserDetails")
+        userReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val data = snapshot.getValue(FirebaseModel::class.java) ?: return
+
+                //put UserName to TextView
+                tvUsername.text = " " + data.name.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(applicationContext, "Failed", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     private fun resetDatabase() {
