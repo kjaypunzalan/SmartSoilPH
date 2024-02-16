79ef32aa2213483f4d440c96e43aa6b3686a6ed8
kjaypunzalan <karljohn.punzalan@gmail.com>
2024-02-17 02:35:37 +0800

    added Fertilizer Recommendation backend

--- app/src/main/java/com/iacademy/smartsoilph/activities/HomeActivity.kt
+++ app/src/main/java/com/iacademy/smartsoilph/activities/HomeActivity.kt
@@ -8,10 +8,12 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
 import android.widget.PopupMenu
+import android.widget.Switch
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.cardview.widget.CardView
+import androidx.core.widget.NestedScrollView
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
@@ -35,11 +37,14 @@ class HomeActivity : BaseActivity() {
     private lateinit var btnWeather: CardView
     private lateinit var btnReports: CardView
     private lateinit var btnManual: CardView
-    private lateinit var btnLogout: CardView
+    private lateinit var btnRecommendationHistory: CardView
     private lateinit var tvUsername: TextView
     private lateinit var tvDateToday: TextView
     private lateinit var btnBtConnect: CardView
     private lateinit var btnSettings: CardView
+    private lateinit var ivBGHome: ImageView
+    private lateinit var btnSwitch: Switch
+    private lateinit var nestedScrollView: NestedScrollView
 
     // Declare Firebase variables
     private lateinit var auth: FirebaseAuth
@@ -62,42 +67,61 @@ class HomeActivity : BaseActivity() {
     }
 
     private fun initializeLayout() {
-
-        btnSoil = findViewById<CardView>(R.id.soil_card)
-        btnWeather = findViewById<CardView>(R.id.weather_card)
-        btnReports = findViewById<CardView>(R.id.reports_card)
-        btnManual = findViewById<CardView>(R.id.manual_card)
-//        btnLogout = findViewById<CardView>(R.id.logout_card)
-        tvUsername = findViewById<TextView>(R.id.tv_username)
-        tvDateToday = findViewById<TextView>(R.id.tv_date_today)
-
-        //settings
-        btnSettings = findViewById<CardView>(R.id.settings_card)
         btnSoil = findViewById(R.id.soil_card)
         btnWeather = findViewById(R.id.weather_card)
         btnReports = findViewById(R.id.reports_card)
+        btnRecommendationHistory = findViewById(R.id.fertilizer_card)
         btnManual = findViewById(R.id.manual_card)
-//        btnLogout = findViewById(R.id.logout_card)
+        btnSettings = findViewById(R.id.settings_card)
+
         tvUsername = findViewById(R.id.tv_username)
         tvDateToday = findViewById(R.id.tv_date_today)
+        btnSwitch = findViewById(R.id.switch_button)
         btnBtConnect = findViewById(R.id.bluetooth_card)
+        ivBGHome = findViewById(R.id.bg_home)
+        nestedScrollView = findViewById(R.id.nested_scrollView)
+
+        // Add Parallax Effect
+        nestedScrollView.viewTreeObserver.addOnScrollChangedListener {
+            val scrollY = nestedScrollView.scrollY // For ScrollView
+            ivBGHome.translationY = scrollY * 0.5f // Adjust parallax effect speed here
+        }
         // Note: btnSyncDatabase and other menu items are handled through onCreateOptionsMenu and onOptionsItemSelected
     }
 
+    //Button Function
+    private fun setButtonClickListener(button: CardView, activityClass: Class<*>) {
+        button.setOnClickListener {
+            val intent = Intent(this, activityClass)
+            startActivity(intent)
+        }
+    }
     private fun setupButtonNavigation() {
         // set click listeners for buttons
-        setButtonClickListener(btnSoil, SoilActivity_test::class.java)
+        setButtonClickListener(btnSoil, SoilActivity::class.java)
         setButtonClickListener(btnWeather, WeatherActivity::class.java)
         setButtonClickListener(btnReports, ReportsActivity::class.java)
-        setButtonClickListener(btnManual, RecommendationHistoryActivity::class.java)
-        setButtonClickListener(btnBtConnect, ArduinoController::class.java) // Ensure BluetoothController exists or adjust as needed
+        setButtonClickListener(btnRecommendationHistory, RecommendationHistoryActivity::class.java)
+        //setButtonClickListener(btnBtConnect, SoilActivity_test::class.java)
+        //setButtonClickListener(btnBtConnect, ArduinoController::class.java) // Ensure BluetoothController exists or adjust as needed
 
-        btnLogout.setOnClickListener {
+        btnBtConnect.setOnClickListener {
+            // Toggle the switch's checked state
+            btnSwitch.isChecked = !btnSwitch.isChecked
+
+            // Optionally, perform actions based on the new state of the switch
+            if (btnSwitch.isChecked) {
+                // Code to execute when the switch is turned ON
+            } else {
+                // Code to execute when the switch is turned OFF
+            }
+        }
+        /*btnLogout.setOnClickListener {
             auth.signOut()
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        }
+        }*/
 
 
         //settings
@@ -161,14 +185,6 @@ class HomeActivity : BaseActivity() {
         }
     }
 
-
-    //Button Function
-    private fun setButtonClickListener(button: CardView, activityClass: Class<*>) {
-        button.setOnClickListener {
-            val intent = Intent(this, activityClass)
-            startActivity(intent)
-        }
-    }
 
     private fun displayCurrentDate() {
         val dateFormat = SimpleDateFormat("EEEE, MMM d", Locale.getDefault())
